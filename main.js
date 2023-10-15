30144d04be3577bf5cee706e3d08147f16f5d192
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2023-10-16 01:23:56 +0500

    feat: add basic UI

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-   Hello world
-  </div>
+let gridMarkup = `
+<div class="grid">
 `;
+
+for (let index = 0; index < 9; index++) {
+  gridMarkup += `
+  <div class="cell" data-cell-id="${index}">
+  </div>
+  `;
+}
+
+gridMarkup += `
+</div>
+`;
+
+const handleClick = (event) => {
+  const clickedEl = event?.target;
+};
+
+document.querySelector("#app").innerHTML = gridMarkup;
+
+document.querySelector(".grid").addEventListener("click", handleClick);
