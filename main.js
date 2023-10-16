c03439b696cc777ba8dab4895b15574673a9017b
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2023-10-17 01:18:54 +0500

    feat: add basic user input and validation

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 import "./style.css";
 
+let grid = [
+  [-1, -1, -1],
+  [-1, -1, -1],
+  [-1, -1, -1],
+];
+
 let gridMarkup = `
 <div class="grid">
 `;
@@ -17,6 +23,15 @@ gridMarkup += `
 
 const handleClick = (event) => {
   const clickedEl = event?.target;
+
+  const cellId = Number(clickedEl?.dataset?.cellId);
+  const row = Math.floor(cellId / 3);
+  const col = cellId % 3;
+
+  if (grid[row][col] === -1) {
+    clickedEl.innerHTML = "X";
+    grid[row][col] = 0;
+  }
 };
 
 document.querySelector("#app").innerHTML = gridMarkup;
